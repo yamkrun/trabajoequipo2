297d8c0189f0c54f4ad57e995217846f928b389e
gnehemias04 <gnehemias04@gmail.com>
2025-07-21 13:48:43 -0300

    first commit gabriel

---
+++ main.js
@@ -0,0 +1,17 @@
+let total;
+function suma(a, b) {
+  total = a + b;
+  return total;
+}
+function resta(a, b) {
+  total = a - b;
+  return total;
+}
+function multiplicacion(a, b) {
+  total = a * b;
+  return total;
+}
+function division(a, b) {
+  total = a / b;
+  return total;
+}
